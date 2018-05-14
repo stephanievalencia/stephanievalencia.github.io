7818f7c567a831e17c2f7ae01e095c0b19192bef
Stephanie Valencia <stephanie.valencia@sjsu.edu>
2018-05-14 14:20:20 -0700

    update

---
+++ js/var canvas;.js
@@ -0,0 +1,25 @@
+var Canvas; 
+function windowResized(){
+	resizeCanvas();
+}
+var barWidth = 50;
+var lastBar = 0;
+
+function setup() {
+  canvas = createCanvas (windowWidth, windowHeight);
+  canvas.position (0, 0);
+  canvas.style ('z-index', '-1');
+  colorMode(HSB, width, height, height);  
+  noStroke();
+  background(0);
+}
+
+function draw() {
+  var whichBar = mouseX / barWidth;
+  if (whichBar !== lastBar) {
+    var barX = whichBar * barWidth;
+    fill(mouseY, height, height);
+    rect(barX, 100, barWidth, height);
+    lastBar = whichBar;
+  }
+}
